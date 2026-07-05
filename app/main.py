67e040aae18dc3b50685e8fce21cc824575d7d9c
Chamith Dilshan <chamithzdilshann@gmail.com>
2026-07-05 18:19:00 +0530

    Add gitignore and chnage api and test cases

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    author: str
+
 
 @app.get("/")
-async def root():
+async def root() -> dict:
     return {"message": "Hello World For real"}
 
+@app.get("/posts")
+async def get_posts()-> dict:
+    return {"message": f"This is a list of posts"}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
+@app.post("/posts")
+async def create_posts(post:Post) -> dict:
+    return {"message": f"{post.author} - Post created!"}
--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,17 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_say_hello():
-    response = client.get("/hello/John")
+def test_root():
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello John"}
+    assert response.json() == {"message": "Hello World For real"}
+
+def test_get_posts():
+    response = client.get("/posts")
+    assert response.status_code == 200
+    assert response.json() == {"message": "This is a list of posts"}
+
+def test_create_posts():
+    response = client.post("/posts", json={"title": "My First Post", "content": "This is my first post", "author": "John"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "John - Post created!"}
